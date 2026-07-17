5fd644f597dc5b8e5329ab7a1e0a30cc72463169
Rei0133 <reychee05@gmail.com>
2026-07-17 22:18:25 +0800

    Update Board.tsx

--- chess-game/src/Board.tsx
+++ chess-game/src/Board.tsx
@@ -27,7 +27,7 @@ interface Props {
 export default function Board({ state, onSquareClick, onPromotion, flipped, showCoordinates, showValidMoves, boardTheme, hintFrom, locked }: Props) {
   const { board, selected, validMoves, promotionPending } = state;
 
-  const rows = flipped ? [0,1,2,3,4,5,6,7] : [7,6,5,4,3,2,1,0];
+  const rows = flipped ? [7,6,5,4,3,2,1,0] : [0,1,2,3,4,5,6,7];
   const cols = flipped ? [7,6,5,4,3,2,1,0] : [0,1,2,3,4,5,6,7];
 
   const isValidMove = (r: number, c: number) => showValidMoves && validMoves.some(([vr, vc]) => vr === r && vc === c);
