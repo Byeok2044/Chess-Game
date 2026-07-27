3db6edf5dfcc765a830b2c9211fb3a8b222cf337
Rei0133 <reychee05@gmail.com>
2026-07-27 23:12:04 +0800

    Vercel Analytics

--- chess-game/src/main.tsx
+++ chess-game/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { Analytics } from '@vercel/analytics/react'
 
 import { AuthProvider } from './lib/AuthContext.tsx';
 
@@ -9,6 +10,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AuthProvider>
       <App />
+      <Analytics />
     </AuthProvider>
   </StrictMode>,
 );
